828c6109315b221035bb4050be7f7b60aa5f524f
Victor Rachev <waterlou5@mail.ru>
2024-03-18 17:31:06 +0300

    все работает

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,22 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    public static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/Waterlou";
+    public static final String DB_USERNAME = "root";
+    public static final String DB_PASSWORD = "Dbn.irf2005";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName(DB_DRIVER);
+            return DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
